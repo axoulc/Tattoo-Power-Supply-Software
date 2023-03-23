5b36c7487609187773b90d709fe39abcd4eae1db
axoulc <axel.chabot@protonmail.com>
2023-03-23 23:07:07 +0100

    Test OK with FreeRTOS

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include "peripherals.h"
+
+void vTaskCode(void *pvParameters);
+
+TaskHandle_t xHandle = NULL;
 
 int main(void) {
 	configure_clock();
@@ -10,10 +17,24 @@ int main(void) {
 	configure_usart();
 	configure_i2c();
 	configure_adc();
-	
+
+	xTaskCreate(vTaskCode, "NAME", 256, (void *)1, tskIDLE_PRIORITY + 1, &xHandle);
+
+	vTaskStartScheduler();
+
 	while (1) {
 		__asm__("nop");
 	}
 
 	return 0;
+}
+
+void vTaskCode(void *pvParameters)
+{
+	int adc_value = 0;
+	for (;;)
+	{
+		adc_value = read_adc_native(0);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 }
