aab73595b130b20f0135e741c5d3b41bc60c2284
axoulc <axel.chabot@protonmail.com>
2023-04-13 23:31:23 +0200

    Good start GUI without mui

--- inc/display.h
+++ inc/display.h
@@ -1,9 +1,15 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <stdint.h>
+
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define TOP_BOX_HEIGHT 12
+
 typedef struct {
-    uint32_t current = 0;
-    uint32_t last = 1;
+    uint32_t current;
+    uint32_t last;
 } encoder_t;
 
 void display_init_task(void);
--- src/display.c
+++ src/display.c
@@ -13,6 +13,7 @@
 void display_task(void *pvParameters);
 void u8x8_byte_4wire_hw_spi_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 void u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+bool handle_event_display(mui_t *ui, encoder_t *encoder);
 
 TaskHandle_t DisplayTaskHandle = NULL;
 
@@ -25,41 +26,55 @@ void display_task(void *pvParameters) {
     uint32_t count = 0, last_count = 0;
     char buffer[16];
     u8g2_t u8g2_i, *u8g2 = &u8g2_i;
+    encoder_t encoder = {0, 0};
     bool is_redraw = true;
 
+/*
     mui_t mui_i, *mui = &mui_i;
+
+    uint8_t number = 0;
   
     muif_t muif_list[] = {
         //MUIF_VARIABLE("BN", NULL, mui_u8g2_btn_exit_wm_fi)};
-        MUIF_U8G2_FONT_STYLE(0, u8g2_font_helvR08_tr),   /* define style 0 */
-        MUIF_U8G2_LABEL(),                               /* allow MUI_LABEL macro */
-        MUIF_BUTTON("BN", mui_u8g2_btn_exit_wm_fi)
+        MUIF_U8G2_FONT_STYLE(0, u8g2_font_helvR08_tr),
+        MUIF_U8G2_LABEL(),
+        MUIF_U8G2_U8_MIN_MAX_STEP("B0", &number, 0, 20, 2, MUI_MMS_2X_BAR|MUI_MMS_SHOW_VALUE, mui_u8g2_u8_bar_wm_mud_pf)
     };
 
     fds_t fds_data[] =
-        //MUI_FORM(1)
-        //MUI_XYT("BN", 64, 30, " Select Me ");
-        MUI_FORM(1)                            /* This will start the definition of form 1 */
-        MUI_STYLE(0)                           /* select the font defined with style 0 */
-        MUI_LABEL(5, 15, "Hello U8g2")         /* place text at postion x=5, y=15 */
-        MUI_XYT("BN",64, 30, " Exit ");  
-
+        MUI_FORM(1)
+        MUI_STYLE(0)
+        MUI_LABEL(1,40, "Bar: ")
+        MUI_XY("B0",40, 40)
+        MUI_GOTO(64, 59, 90, " Ok ")
+        ;
+*/
     u8g2_Setup_sh1106_128x64_noname_1(u8g2, U8G2_R0, u8x8_byte_4wire_hw_spi_stm32, u8x8_gpio_and_delay_stm32);
     u8g2_InitDisplay(u8g2);
     u8g2_SetPowerSave(u8g2, 0);
-    //u8g2_SetFont(u8g2, u8g2_font_helvR08_tr);
-
-    mui_Init(mui, u8g2, fds_data, muif_list, sizeof(muif_list)/sizeof(muif_t));
-    mui_GotoForm(mui, 1, 0);
+    u8g2_SetFont(u8g2, u8g2_font_helvR08_tr);
+    u8g2_SetFontMode(u8g2, 0);
+    //mui_Init(mui, u8g2, fds_data, muif_list, sizeof(muif_list)/sizeof(muif_t));
+    //mui_GotoForm(mui, 1, 0);
 
     for (;;) {
         if (is_redraw) {
             u8g2_FirstPage(u8g2);
             do {
-                mui_Draw(mui);
+                u8g2_DrawFrame(u8g2, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+                u8g2_DrawHLine(u8g2, 0, TOP_BOX_HEIGHT, DISPLAY_WIDTH);
+                u8g2_DrawVLine(u8g2, 64, TOP_BOX_HEIGHT, DISPLAY_HEIGHT - TOP_BOX_HEIGHT);
+                u8g2_SetDrawColor(u8g2, 1);
+                u8g2_DrawStr(u8g2, 2, TOP_BOX_HEIGHT - 2, "Tattoo Machine");
+                u8g2_SetDrawColor(u8g2, 0);
+                u8g2_DrawStr(u8g2, 106, TOP_BOX_HEIGHT - 2, "20V");
+                u8g2_SetDrawColor(u8g2, 1);
+                //mui_Draw(mui);
+            
             } while (u8g2_NextPage(u8g2));
             is_redraw = false;
         }
+        //is_redraw = handle_event_display(mui, &encoder);
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
 }
@@ -114,12 +129,25 @@ void u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void 
     return 1;
 }
 
-void handle_event_display(mui_t *ui, encoder_t *encoder) {
-    if (get_encoder_sw()) {
-
+bool handle_event_display(mui_t *ui, encoder_t *encoder) {
+    bool ret = false;
+    if (!get_encoder_sw()) {
+        mui_SendSelect(ui);
+        ret = true;
     }
-    if (get_encoder_clk()) {
-        if ()
-        mui_Next(ui);
+    if (!get_encoder_clk()) {
+        encoder->current = get_encoder_count() >> 2;
+        if (encoder->current == encoder->last) {
+            return ret;
+        } else if ((encoder->current - encoder->last) > 0) {
+            mui_NextField(ui);
+            encoder->last = encoder->current;
+            ret = true;
+        } else if ((encoder->current - encoder->last) < 0) {
+            mui_PrevField(ui);
+            encoder->last = encoder->current;
+            ret = true;
+        }
     }
+    return ret;
 }
